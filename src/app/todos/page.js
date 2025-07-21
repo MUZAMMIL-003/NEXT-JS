fb1ea603677064d2f22becc0da70904dd5ad3d1e
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-07-22 00:53:34 +0500

    first commit

---
+++ src/app/todos/page.js
@@ -0,0 +1,10 @@
+export default function Todos() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100"> 
+     
+      <h1 className="text-3xl text-center font-bold ">Todos</h1>
+      <p>This is the Todos page.</p>
+    </div>
+
+  );
+}
