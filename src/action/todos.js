15b38f3cadb1abb0bc557ee49688c464a2d3d169
MUZAMMIL-003 <muhammadmuzzi90@gmail.com>
2025-07-23 02:00:53 +0500

    .

--- src/action/todos.js
+++ src/action/todos.js
@@ -18,53 +18,53 @@ import { revalidatePath } from "next/cache";
 // }
 
 export async function addTodo(formData) {
-  const todo = formData.get("todo");
-  console.log("adding todo:", todo); // Debug
+    const todo = formData.get("todo");
+    console.log("adding todo:", todo); // Debug
 
-  try {
-    await fetch("http://localhost:3000/api/todos", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json", // Important!
-      },
-      body: JSON.stringify({ todo }),
-    });
+    try {
+        await fetch("http://localhost:3000/api/todos", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json", // Important!
+            },
+            body: JSON.stringify({ todo }),
+        });
 
-    revalidatePath("/todos");
-  } catch (err) {
-    console.log("Error adding todo:", err);
-  }
+        revalidatePath("/todos");
+    } catch (err) {
+        console.log("Error adding todo:", err);
+    }
 }
 
 export async function updateTodo(obj) {
-  console.log("obj==in update todo=>", obj);
-  try {
-    await fetch("http://localhost:3000/api/todos", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(obj),
-    });
+    console.log("obj==in update todo=>", obj);
+    try {
+        await fetch("http://localhost:3000/api/todos", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj),
+        });
 
-    revalidatePath("/todos");
-  } catch (err) {
-    console.log(err);
-  }
+        revalidatePath("/todos");
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 export async function deleteTodo(obj) {
-  try {
-    await fetch("http://localhost:3000/api/todos", {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(obj),
-    });
+    try {
+        await fetch("http://localhost:3000/api/todos", {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(obj),
+        });
 
-    revalidatePath("/todos");
-  } catch (err) {
-    console.log(err);
-  }
+        revalidatePath("/todos");
+    } catch (err) {
+        console.log(err);
+    }
 }
